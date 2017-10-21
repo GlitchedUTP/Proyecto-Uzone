fc0328e7570d629c8b22523c5d9e9d740bcaf1b8
Luis Torres <job.soft.1995@gmail.com>
2017-10-21 02:10:36 -0500

    Clase information e informationEntity creado

---
+++ Codificacion/UZone/src/pe/edu/utp/uzonewebprofile/models/Information.java
@@ -0,0 +1,66 @@
+package pe.edu.utp.uzonewebprofile.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Information {
+    int info_id;
+    String info_description;
+    double info_avgPoints;
+    int info_voteQuantity;
+
+    public Information(int info_id, String info_description, double info_avgPoints, int info_voteQuantity) {
+        this.info_id = info_id;
+        this.info_description = info_description;
+        this.info_avgPoints = info_avgPoints;
+        this.info_voteQuantity = info_voteQuantity;
+    }
+
+    public Information() {
+
+    }
+    public int getInfo_id() {
+        return info_id;
+    }
+
+    public void setInfo_id(int info_id) {
+        this.info_id = info_id;
+    }
+
+    public String getInfo_description() {
+        return info_description;
+    }
+
+    public void setInfo_description(String info_description) {
+        this.info_description = info_description;
+    }
+
+    public double getInfo_avgPoints() {
+        return info_avgPoints;
+    }
+
+    public void setInfo_avgPoints(double info_avgPoints) {
+        this.info_avgPoints = info_avgPoints;
+    }
+
+    public int getInfo_voteQuantity() {
+        return info_voteQuantity;
+    }
+
+    public void setInfo_voteQuantity(int info_voteQuantity) {
+        this.info_voteQuantity = info_voteQuantity;
+    }
+
+    public static Information from (ResultSet rs) {
+        try {
+            return new Information(
+                    rs.getInt("info_id"),
+                    rs.getString("info_description"),
+                    rs.getDouble("info_avgPoints"),
+                    rs.getInt("info_voteQuantity"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ Codificacion/UZone/src/pe/edu/utp/uzonewebprofile/models/InformationEntity.java
@@ -0,0 +1,9 @@
+package pe.edu.utp.uzonewebprofile.models;
+
+public class InformationEntity extends BaseEntity{
+    public InformationEntity() {
+        super();
+        setTableName("Information");
+    }
+
+}
