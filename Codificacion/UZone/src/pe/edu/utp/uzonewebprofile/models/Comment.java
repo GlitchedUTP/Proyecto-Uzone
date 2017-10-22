df494b47dcbfbd150966f7e34cdcf0f7796cd9c5
Jordan Lozano <32384017+bealone01@users.noreply.github.com>
2017-10-21 19:16:58 -0500

    seguimos creando CLass COMMENT

--- Codificacion/UZone/src/pe/edu/utp/uzonewebprofile/models/Comment.java
+++ Codificacion/UZone/src/pe/edu/utp/uzonewebprofile/models/Comment.java
@@ -1,4 +1,19 @@
 package pe.edu.utp.uzonewebprofile.models;
 
 public class Comment extends BaseEntity {
+
+    private int id;
+    private String comment;
+
+    public Comment(int id, String comment) { this.id = id;this.comment = comment; }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getComment() { return comment; }
+
+    public void setComment(String comment) { this.comment = comment; }
+
+     
 }
