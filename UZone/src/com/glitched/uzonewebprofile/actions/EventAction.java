4311d5a58fc8182088c4b1346c9c8dc93d506c3d
ExoBlod <envyxwrath@outlook.com.pe>
2017-11-11 18:53:04 -0500

    Correcion

--- UZone/src/com/glitched/uzonewebprofile/actions/EventAction.java
+++ UZone/src/com/glitched/uzonewebprofile/actions/EventAction.java
@@ -6,7 +6,7 @@ import com.opensymphony.xwork2.ModelDriven;
 
 public class EventAction extends ActionSupport implements ModelDriven<Event> {
     private Event event= new Event();
-    public Event getModel(){return event}
+    public Event getModel(){return event;}
 
     @Override
     public String execute() throws Exception{return SUCCESS;}
