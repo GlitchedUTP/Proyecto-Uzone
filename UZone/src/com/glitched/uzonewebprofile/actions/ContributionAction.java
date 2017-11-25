0a4a7d43ce43dd486f8cd10d5e9c628a178cb33b
Jordan Lozano <32384017+bealone01@users.noreply.github.com>
2017-11-25 02:37:29 -0500

    contributios jsp y action

--- UZone/src/com/glitched/uzonewebprofile/actions/ContributionAction.java
+++ UZone/src/com/glitched/uzonewebprofile/actions/ContributionAction.java
@@ -1,4 +1,111 @@
 package com.glitched.uzonewebprofile.actions;
 
-public class ContributionAction {
+import com.glitched.uzonewebprofile.models.Event;
+import com.glitched.uzonewebprofile.models.Post;
+import com.glitched.uzonewebprofile.models.UZoneService;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.List;
+import java.util.Map;
+
+public class ContributionAction extends ActionSupport implements ModelDriven<Post> {
+    private  Post post = new Post();
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Map<String, Object> getSessionMap() {
+        return sessionMap;
+    }
+
+    public void setSessionMap(Map<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    private Map<String, Object> sessionMap;
+    private List<Post> posts;
+
+
+    @Override
+    public Post getModel() {
+        return null;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        UZoneService service = new UZoneService();
+        if (sessionMap.containsKey("username")) {
+            post.setUser(service.findUserById((int)sessionMap.get("id")));
+
+            if (service.createPost(post)) {
+                setPosts(service.findLastest());
+                return SUCCESS;
+            }
+            else return ERROR;
+        } else return ERROR;
+    }
 }
+
+
+
+/*
+public class EventAction extends ActionSupport implements ModelDriven<Event>, SessionAware {
+    private Event event= new Event();
+    private Map<String, Object> sessionMap;
+    private List<Event> events;
+    private List<Post> posts;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {this.events= events; }
+
+    @Override
+    public Event getModel(){return event;}
+
+    @Override
+    public void setSession(Map<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        UZoneService service = new UZoneService();
+        if (sessionMap.containsKey("username")) {
+            event.setUser(service.findUserById((int)sessionMap.get("id")));
+            if(event.getPictureUrl()==null) {
+                event.setPictureUrl("default.jpg");
+            }
+            if (service.createEvent(event)) {
+                setPosts(service.findLastest());
+                return SUCCESS;
+            }
+            else return ERROR;
+        } else return ERROR;
+    }
+}
+*/
